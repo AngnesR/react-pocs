aaf427573d99e92fdcf3fb167a2b95cdd6672f21
renan <renan.rl121@gmail.com>
2022-04-02 11:25:00 -0300

    header content e footer

--- renan-site/src/App.js
+++ renan-site/src/App.js
@@ -4,29 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <div className="Title">
-          <p>
-            site do renan
-          </p>
-        </div>
-        <div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
+        <p>
+          site do renan
+        </p>
+      </header >
+
+      <div className="app-content">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <div>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
+      </div>
 
+      <footer className="App-footer">
 
-
-      </header >
+        <p>
+          esse site foi feito no react
+        </p>
+      </footer>
     </div >
   );
 }
